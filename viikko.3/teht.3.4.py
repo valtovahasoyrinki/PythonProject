2be65dc2ff77cd284e6c02191d7284805e07b5ab
valtovahasoyrinki <valto.vahasoyrinki@metropolia.fi>
2026-02-11 15:50:37 +0200

    Vko 3 tehtävät

--- viikko.3/teht.3.4.py
+++ viikko.3/teht.3.4.py
@@ -2,11 +2,13 @@ vuosiluku=int(input("Sano joku vuosiluku"))
 if vuosiluku <0:
     print("Sano psoitiivinen vuosiluku")
 if vuosiluku >0:
-    if vuosiluku % 4==0:
-        print(f"vuonna {vuosiluku} oli karkauspäivä.")
-    else:
+    if vuosiluku % 400==0:
         print(f"vuonna {vuosiluku} oli karkauspäivä")
-
-if vuosiluku %100==0
+    elif vuosiluku % 100 == 0:
+        print(f"vuonna {vuosiluku} ei ollut karkauspäivä")
+    elif vuosiluku % 4 == 0:
+        print(f"vuonna {vuosiluku} oli karkauspäivä")
+    else:
+        print(f"vuonna {vuosiluku} ei ollut karkauspäivä")
 
 
