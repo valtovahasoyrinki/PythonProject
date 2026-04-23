9245ee1b21d7c32dc0f8ce8f9830561020158878
valtovahasoyrinki <valto.vahasoyrinki@metropolia.fi>
2026-04-23 10:21:33 +0300

    vko tehtävät

--- viikko.10/kertaustehtävät.4.py
+++ viikko.10/kertaustehtävät.4.py
@@ -11,11 +11,11 @@ x1 = float(input("Sano x1: "))
 y1 = float(input("Sano y1: "))
 piste1 = create_point(x1, y1)
 
-print("\nAnna toisen pisteen koordinaatit:")
+print("Anna toisen pisteen koordinaatit:")
 x2 = float(input("Sano x2: "))
 y2 = float(input("Sano y2: "))
 piste2 = create_point(x2, y2)
 
 etaisyys = distance(piste1, piste2)
 
-print(f"\nPisteiden välinen etäisyys on: {etaisyys:.2f}")
+print("Pisteiden väli on :", etaisyys)
